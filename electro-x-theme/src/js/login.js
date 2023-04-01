f857ac5a111b63ad943c008f304469525da21697
cumsud147147 <c.smayilov1@gmail.com>
2023-04-01 09:00:12 -0700

    login and register page add

---
+++ electro-x-theme/src/js/login.js
@@ -0,0 +1,64 @@
+// Slide js code start
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+   
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 6,
+        spaceBetween: 35,
+      },
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+  // Slide js code end
+
+  // Totop code star
+  const ToTop = document.querySelector(".totop");
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 500) {
+      if (ToTop) {
+        ToTop.style.display = "flex";
+       
+       
+          
+          
+      }
+    } else {
+      document.querySelector("body").style.background = "white";
+      if (ToTop) {
+        ToTop.style.display = "none";
+      }
+    }
+  });
+  ToTop &&
+    ToTop.addEventListener("click", () => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+  // Totop code end
+
+  // Navbar scrool code js start
+const Navbar = document.querySelector(".nav-top");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    if (Navbar) {
+      Navbar.classList.add("actice-nav");
+    }
+  } else {
+    if (Navbar) {
+      Navbar.classList.remove("actice-nav");
+    }
+  }
+});
+// Navbar scrool code js end
