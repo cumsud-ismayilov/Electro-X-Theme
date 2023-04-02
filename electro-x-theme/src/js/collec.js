38421d1f88525bb5e210e42af454bc0badb6e35f
cumsud147147 <c.smayilov1@gmail.com>
2023-04-02 07:09:38 -0700

    detail page nav and footer added

--- electro-x-theme/src/js/collec.js
+++ electro-x-theme/src/js/collec.js
@@ -23,3 +23,42 @@ var swiper = new Swiper(".mySwiper", {
       },
   });
 // Slider code end
+
+// Collection ToTop code start
+const ToTop = document.querySelector(".totop");
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 500) {
+      if (ToTop) {
+        ToTop.style.display = "flex";
+       
+       
+          
+          
+      }
+    } else {
+      document.querySelector("body").style.background = "white";
+      if (ToTop) {
+        ToTop.style.display = "none";
+      }
+    }
+  });
+  ToTop &&
+    ToTop.addEventListener("click", () => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+// Collection ToTop code end
+
+// Navbar scrool code start
+const Navbar = document.querySelector(".nav-top");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    if (Navbar) {
+      Navbar.classList.add("actice-nav");
+    }
+  } else {
+    if (Navbar) {
+      Navbar.classList.remove("actice-nav");
+    }
+  }
+});
+// Navbar scrool code end
--- electro-x-theme/src/js/detail.js
+++ electro-x-theme/src/js/detail.js
@@ -1,4 +1,28 @@
-
+// Slider code start
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+   
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 6,
+        spaceBetween: 35,
+      },
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+// Slider code end
 
 
 
