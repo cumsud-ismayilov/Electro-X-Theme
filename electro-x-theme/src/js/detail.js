8a7fcc93f47bba3266b42c55c0d011a58dfc359c
cumsud147147 <c.smayilov1@gmail.com>
2023-04-03 11:37:37 -0700

    tumb galery code add

--- electro-x-theme/src/js/detail.js
+++ electro-x-theme/src/js/detail.js
@@ -25,7 +25,24 @@ var swiper = new Swiper(".mySwiper", {
 // Slider code end
 
 // Tumb galery code start
-
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
   
 // Tumb galery code end
 
