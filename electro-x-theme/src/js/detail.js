3076f5233755a8c32330668edec2e9c4ea0b0602
cumsud147147 <c.smayilov1@gmail.com>
2023-04-05 08:13:24 -0700

    Detail page slider 2 code add

--- electro-x-theme/src/js/detail.js
+++ electro-x-theme/src/js/detail.js
@@ -17,16 +17,32 @@ var swiper = new Swiper(".mySwiper", {
         spaceBetween: 35,
       },
     },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
+    // navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    //   },
   });
 // Slider code end
 
 // Tumb galery code start
-
-  
+var swipers = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2s = new Swiper(".mySwiper2", {
+  // loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swipers: swiper2s,
+  },
+});
 // Tumb galery code end
 
 
